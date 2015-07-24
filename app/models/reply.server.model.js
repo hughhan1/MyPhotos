96a43fd7336abc985ebe946882fdbbfa95e03d80
Hugh Han <hughhan1@gmail.com>
2015-07-24 19:50:46 -0400

    reply form mongoose validation

--- app/models/reply.server.model.js
+++ app/models/reply.server.model.js
@@ -1,9 +1,27 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var validator = require('validator');
+var Schema = mongoose.Schema;
 
 var ReplySchema = new Schema({
-    name: String,
-    email: String,
+    firstName: {
+    	type: String,
+    	trim: true,
+    	required: 'First name is required.',
+    	validate: [ validator.isAlpha, 'FIrst name is required (A-z).']
+    },
+    lastName: {
+    	type: String,
+    	trim: true,
+    	required: 'Last name is required.',
+    	validate: [ validator.isAlpha, 'Last name is required (A-z).']
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: 'Email is required.',
+        validate: [ validator.isEmail, 'Invalid email.' ],
+    },
     comment: String
 });
 
